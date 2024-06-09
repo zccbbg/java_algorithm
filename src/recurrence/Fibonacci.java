6cf8546527988c65c30ab9ed609ff6b9f1623c30
zccbbg <zccbbg@qq.com>
2024-06-09 23:30:41 +0800

    例3.2 斐波那契数列

---
+++ src/recurrence/Fibonacci.java
@@ -0,0 +1,16 @@
+package recurrence;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int f0 = 1, f1 = 1, f2;
+        int n = 20;
+        System.out.print(f0 + " " + f1 + " ");
+        for (int i = 3; i <= n; i++) {
+            f2 = f0 + f1;
+            System.out.print(f2 + " ");
+            f0 = f1;
+            f1 = f2;
+        }
+    }
+}
+
